33c6f60ab2fbe9c35fe8076c2d03e64d65ae0db2
tgraham-ucsc <tgraham@ucsc.edu>
2021-11-18 20:22:39 +0000

    changed project layout and added snapshots

---
+++ dbt-greenery/snapshots/addresses.sql
@@ -0,0 +1,16 @@
+{% snapshot addresses_snapshot %}
+
+  {{
+    config(
+      target_schema='snapshots',
+      unique_key='address_id',
+
+      strategy='check',
+      check_cols=['address', 'zipcode', 'state','country']
+    )
+  }}
+
+  SELECT * 
+  FROM {{ source('tutorial', 'addresses') }}
+
+{% endsnapshot %}

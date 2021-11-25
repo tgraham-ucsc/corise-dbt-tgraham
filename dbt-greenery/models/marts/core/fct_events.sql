5160fa228a35d9fc2b7d60f69500b31326a8d57f
tgraham-ucsc <tgraham@ucsc.edu>
2021-11-25 02:52:53 +0000

    adding fct_events

---
+++ dbt-greenery/models/marts/core/fct_events.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+
+select 
+   e.event_id,
+   e.session_id,
+   e.user_id,
+   e.page_url,
+   e.event_date,
+   e.event_type,
+   e.product_id,
+   p.product_name,
+   p.product_price
+from {{ ref('stg_events') }} e
+left join {{ ref('stg_products') }} p
+  on e.product_id = p.product_id

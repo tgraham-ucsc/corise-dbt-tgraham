5ec701b55693298ffe5e314180d7028b805e1637
tgraham-ucsc <tgraham@ucsc.edu>
2021-11-25 02:38:25 +0000

    added product_id from page_url

--- dbt-greenery/models/stage/stg_events.sql
+++ dbt-greenery/models/stage/stg_events.sql
@@ -9,6 +9,7 @@ SELECT id,
     session_id,
     user_id,
     page_url,
-    created_at,
-    event_type
+    created_at as event_date,
+    event_type,
+    cast(reverse(split_part(reverse(page_url), '/', 1)) as VARCHAR) as product_id 
 FROM {{ source('tutorial', 'events') }}

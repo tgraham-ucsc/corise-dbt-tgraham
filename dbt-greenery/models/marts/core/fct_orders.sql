4288ed262d77ad8b3532f1489b68bc195d6c82f3
tgraham-ucsc <tgraham@ucsc.edu>
2021-11-25 01:07:59 +0000

    reworked user_orders_fact

--- dbt-greenery/models/marts/core/fct_orders.sql
+++ dbt-greenery/models/marts/core/fct_orders.sql
@@ -8,8 +8,9 @@
 select 
   o.order_id,
   o.user_id,
-  oi.product_id,
   o.order_date,
+  o.order_cost,
+  oi.product_id,
   oi.order_quantity
 from {{ ref('stg_orders') }} o
 left join {{ ref('stg_order_items') }} oi
---
+++ dbt-greenery/models/marts/marketing/user_order_facts.sql
@@ -0,0 +1,36 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+
+with user_orders as (
+
+select count(order_id) as total_orders, 
+  max(order_date) as most_recent_order, 
+  min(order_date) as first_order,
+  sum(round (cast( order_cost as numeric),2)) as total_spend,
+  user_id
+from {{ ref('fct_orders') }}
+  group by user_id
+),
+
+users as (
+  select *
+  from {{ ref('dim_users') }}
+),
+
+  final as (
+  select 
+    user_orders.total_orders,
+    user_orders.total_spend,
+    user_orders.first_order,
+    user_orders.most_recent_order,
+    users.full_name
+  from user_orders 
+    left join users using (user_id)
+    
+)
+
+select * from final

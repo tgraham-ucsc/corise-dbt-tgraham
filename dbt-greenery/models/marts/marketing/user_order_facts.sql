e688c3c1601225d945f8267b439f005b39e4de08
tgraham-ucsc <tgraham@ucsc.edu>
2021-11-28 00:33:22 +0000

    minor improvements - added days_to_deliver

--- dbt-greenery/models/marts/marketing/user_order_facts.sql
+++ dbt-greenery/models/marts/marketing/user_order_facts.sql
@@ -4,16 +4,16 @@
   )
 }}
 
-
 with user_orders as (
 
   select count(order_id) as total_orders, 
     max(order_date) as most_recent_order, 
     min(order_date) as first_order,
     sum(round (cast( order_cost as numeric),2)) as total_spend,
+    extract(day from actual_delivery_date - order_date) as days_to_deliver ,
     user_id
   from {{ ref('fct_orders') }}
-    group by user_id
+    group by user_id, days_to_deliver
   ),
 
 users as (
@@ -27,6 +27,7 @@ final as (
     user_orders.total_spend,
     user_orders.first_order,
     user_orders.most_recent_order,
+    user_orders.days_to_deliver,
     users.full_name
   from user_orders 
     left join users using (user_id)
--- dbt-greenery/models/marts/product/product_event_facts.sql
+++ dbt-greenery/models/marts/product/product_event_facts.sql
@@ -11,10 +11,10 @@ select
   product_price,
   min(event_date) as first_view,
   max(event_date) as last_view,
-  age(
-    date_trunc('day', max(event_date)),
-    date_trunc('day', min(event_date))
-  ) as time_between,
+extract(
+        day
+        from max(event_date) - min(event_date)
+    ) as days_between,
   {{ dbt_utils.pivot(
     'event_type',
     dbt_utils.get_column_values(ref('int_product_events'), 'event_type')

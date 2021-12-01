8e30d3cc9e79cb26909aa50c2db869c76425832d
tgraham-ucsc <tgraham@ucsc.edu>
2021-12-01 01:54:53 +0000

    minor changes

---
+++ dbt-greenery/project_resources/week_three_questions.md
@@ -0,0 +1,14 @@
+
+### conversion rate
+
+```The conversion rate is the number of conversions divided by the total number of visitors. For example, if an ecommerce site receives 200 visitors in a month and has 50 sales, the conversion rate would be 50 divided by 200, or 25%.
+```
+
+
+### What is our overall conversion rate?
+400 total orders
+3355 total events
+130 total users
+
+
+### What is our conversion rate by product?
--- dbt-greenery/project_resources/working.sql
+++ dbt-greenery/project_resources/working.sql
@@ -1,30 +1,12 @@
-with orders_by_products as (
-
-    select 
-     order_id,
-     product_id,
-     order_cost,
-     order_total
-    from fct_orders
-     
-     
-
-
-
-
-select 
-  du.full_name,
-  dp.product_name,
-  count(distinct order_id) total_orders,
-  sum(fo.order_cost),
-  sum(fo.order_total),
-  du.zipcode
-  
-from dbt_tgraham.fct_orders fo
-join dbt_tgraham.dim_products dp
-  on fo.product_id = dp.product_id
-join dbt_tgraham.dim_users du  
-  on fo.user_id = du.user_id
-group by fo.product_id, product_name, full_name, zipcode
-order by full_name
-
+select
+    du.full_name, 
+     e.page_url,
+    e.event_type,
+    o.order_id,
+    o.order_total,
+    o.status
+from public.orders o
+    join public.events e
+    on e.user_id = o.user_id
+    join dbt_tgraham.dim_users du
+    on e.user_id = du.user_id
